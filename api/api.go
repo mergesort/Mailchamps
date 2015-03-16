9d56f90472dbd9e1e926ac4d49553a538a3840b3
Joe Fabisevich <Joe@fabisevi.ch>
2015-03-16 17:57:04 -0400

    Initial commit

---
+++ api/api.go
@@ -0,0 +1,95 @@
+//  api.go
+//  Mailchamp
+//
+//  Created by Joe Fabisevich on 3/16/15
+
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/mattbaird/gochimp"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// JSON marshalling
+
+type SubscriptionInput struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type SubscriptionOutput struct {
+	Email      string `json:"email"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"status"`
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SubscriptionOutput
+
+func (output SubscriptionOutput) SendSubscriptionResponse(rw http.ResponseWriter) {
+	bytes, err := json.Marshal(output)
+
+	if err != nil {
+		log.Fatal("An error occurred marshalling the JSON: ", err)
+	}
+
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.Write(bytes)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// API
+
+func AddNewsletterSubscriber(rw http.ResponseWriter, req *http.Request, input SubscriptionInput) {
+	apiKey := os.Getenv("MAILCHIMP_API_KEY")
+
+	if len(apiKey) == 0 {
+		log.Fatal("Missing Mailchimp API key! Ooh ooh ah ah!")
+	}
+
+	api := gochimp.NewChimp(apiKey, true)
+
+	log.Println("Adding subscriber " + input.Username + "/" + input.Email)
+
+	mergeVars := make(map[string]interface{})
+	mergeVars["USERNAME"] = input.Username
+
+	email := gochimp.Email{
+		Email: input.Email,
+	}
+
+	newsletterId := os.Getenv("NEWSLETTER_ID")
+
+	subscriber := gochimp.ListsSubscribe{
+		ApiKey:           apiKey,
+		ListId:           newsletterId,
+		Email:            email,
+		MergeVars:        mergeVars,
+		EmailType:        "html",
+		DoubleOptIn:      false,
+		UpdateExisting:   true,
+		ReplaceInterests: true,
+		SendWelcome:      false,
+	}
+
+	email, err := api.ListsSubscribe(subscriber)
+
+	if err != nil {
+		log.Fatal("An error occurred subscribing "+input.Username+" to the list: ", err)
+	}
+
+	output := SubscriptionOutput{
+		Message:    input.Username + " has been subscribed",
+		StatusCode: 200,
+		Email:      input.Email,
+	}
+
+	output.SendSubscriptionResponse(rw)
+
+	log.Println("Sent response")
+}
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"mailchamps/api"
+
+	"github.com/sqs/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	timeoutHandler := http.TimeoutHandler(router, time.Second*3, "Timeout!")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+		fmt.Println("missing port")
+	}
+
+	log.Println("Starting server...")
+
+	router.HandleFunc("/subscribe", func(writer http.ResponseWriter, request *http.Request) {
+
+		decoder := json.NewDecoder(request.Body)
+		var input api.SubscriptionInput
+		err := decoder.Decode(&input)
+
+		if len(input.Email) == 0 || len(input.Username) == 0 {
+			var message string
+			if len(input.Email) == 0 && len(input.Username) == 0 {
+				message = "Incorrect parameters"
+			} else if len(input.Email) == 0 {
+				message = "Missing email"
+			} else if len(input.Username) == 0 {
+				message = "Missing username"
+			}
+			incorrectParametersOutput := api.SubscriptionOutput{
+				Email:      input.Email,
+				Message:    message,
+				StatusCode: 409,
+			}
+
+			incorrectParametersOutput.SendSubscriptionResponse(writer)
+		} else if err != nil {
+			errorOutput := api.SubscriptionOutput{
+				Email:      input.Email,
+				Message:    "Incorrect parameters",
+				StatusCode: 400,
+			}
+
+			errorOutput.SendSubscriptionResponse(writer)
+		} else {
+			api.AddNewsletterSubscriber(writer, request, input)
+		}
+	}).Methods("POST")
+
+	http.ListenAndServe(":"+port, timeoutHandler)
+}
